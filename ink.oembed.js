f215fd0dccc1ffae9e68f903b54f749fd5c53604
rogeriopvl <rogeriopvl@gmail.com>
2014-05-13 23:31:19 +0100

    Removed uneeded closure

--- ink.oembed.js
+++ ink.oembed.js
@@ -1,53 +1,51 @@
-(function (Ink) {
+Ink.createExt('OEmbed', 1, ['Ink.Net.Ajax_1', 'Ink.Dom.Element_1'],
+              function (Ajax, InkElement) {
+
     'use strict';
 
-    Ink.createExt('OEmbed', 1, ['Ink.Net.Ajax_1', 'Ink.Dom.Element_1'],
-                  function (Ajax, InkElement) {
-
-        var getProvider = function (url) {
-            var providers = {
-                embedly: 'http://api.embed.ly/v1/api/oembed',
-                sapovideos: 'http://videos.sapo.pt/oembed'
-            };
-
-            if (/videos\.sapo\.pt/.test(url)) {
-                return providers.sapovideos;
-            } else {
-                return providers.embedly;
-            }
+    var getProvider = function (url) {
+        var providers = {
+            embedly: 'http://api.embed.ly/v1/api/oembed',
+            sapovideos: 'http://videos.sapo.pt/oembed'
         };
 
-        var OEmbed = function (holder, opts) {
-            this.holder = holder;
-            this.holderData = InkElement.data(this.holder);
-            this.opts = opts || {};
-            this.opts.endpoint = this.opts.endpoint || getProvider(this.holderData.url);
+        if (/videos\.sapo\.pt/.test(url)) {
+            return providers.sapovideos;
+        } else {
+            return providers.embedly;
+        }
+    };
 
-            this._init();
-        };
+    var OEmbed = function (holder, opts) {
+        this.holder = holder;
+        this.holderData = InkElement.data(this.holder);
+        this.opts = opts || {};
+        this.opts.endpoint = this.opts.endpoint || getProvider(this.holderData.url);
 
-        OEmbed.prototype = {
-            _init: function () {
-                var that = this;
-                console.log(this.opts);
-                this._fetch(this.opts.endpoint, function (data) {
-                    that.holder.innerHTML = data.html;
-                });
+        this._init();
+    };
 
-            },
+    OEmbed.prototype = {
+        _init: function () {
+            var that = this;
+            console.log(this.opts);
+            this._fetch(this.opts.endpoint, function (data) {
+                that.holder.innerHTML = data.html;
+            });
 
-            _fetch: function (url, cb) {
-                return new Ajax(url, {
-                    cors: true,
-                    method: 'GET',
-                    parameters: this.holderData,
-                    onSuccess: function (xhr) {
-                        cb(xhr.responseJSON);
-                    }
-                });
-            }
-        };
+        },
 
-        return OEmbed;
-    });
-})(Ink);
+        _fetch: function (url, cb) {
+            return new Ajax(url, {
+                cors: true,
+                method: 'GET',
+                parameters: this.holderData,
+                onSuccess: function (xhr) {
+                    cb(xhr.responseJSON);
+                }
+            });
+        }
+    };
+
+    return OEmbed;
+});

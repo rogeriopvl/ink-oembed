e514ceec244c68ba0a984062bcc0a770ecee1f5b
rogeriopvl <rogeriopvl@gmail.com>
2014-05-14 20:44:11 +0100

    Add support for oembed photo and rich resource types. Add some jsdoc.

--- ink.oembed.js
+++ ink.oembed.js
@@ -3,6 +3,13 @@ Ink.createExt('OEmbed', 1, ['Ink.Net.Ajax_1', 'Ink.Dom.Element_1'],
 
     'use strict';
 
+    /**
+     * Tries to find the right provider based on given URL
+     *
+     * @param {string} url - the url of the resource to be embedded
+     *
+     * @return {string} the url of the adequate provider for given URL
+     */
     var getProvider = function (url) {
         var providers = {
             embedly: 'http://api.embed.ly/v1/api/oembed',
@@ -16,6 +23,12 @@ Ink.createExt('OEmbed', 1, ['Ink.Net.Ajax_1', 'Ink.Dom.Element_1'],
         }
     };
 
+    /**
+     * @constructor
+     *
+     * @param {object} holder - the DOM element that will contain the embed
+     * @param {object} opts - a hash containing oembed configuration options
+     */
     var OEmbed = function (holder, opts) {
         this.holder = holder;
         this.holderData = InkElement.data(this.holder);
@@ -28,9 +41,18 @@ Ink.createExt('OEmbed', 1, ['Ink.Net.Ajax_1', 'Ink.Dom.Element_1'],
     OEmbed.prototype = {
         _init: function () {
             var that = this;
-            console.log(this.opts);
             this._fetch(this.opts.endpoint, function (data) {
-                that.holder.innerHTML = data.html;
+                if (data.type === 'rich' || data.type === 'video') {
+                    that.holder.innerHTML = data.html;
+                } else if (data.type === 'photo') {
+                    that.holder.src = data.url;
+                } else {
+                    // TODO
+                }
+
+                if (typeof that.opts.callback === 'function') {
+                    that.opts.callback(data);
+                }
             });
 
         },
@@ -46,6 +68,5 @@ Ink.createExt('OEmbed', 1, ['Ink.Net.Ajax_1', 'Ink.Dom.Element_1'],
             });
         }
     };
-
     return OEmbed;
 });
